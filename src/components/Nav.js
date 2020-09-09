7f9db9fbbdf05482f25ac8cd4572eaf9fa4b7c79
ckmonish2000 <ckmonish2000@gmail.com>
2020-09-09 18:43:57 +0530

    working routes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav>
       <ul>
-        <li>Home </li>
-        <li>About</li>
+        <li>
+          <Link to="/shop">Shop</Link>{" "}
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
